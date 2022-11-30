e640ea07b2e7f1fffe6684f92b1191bd6ebc31ff
dpmanek <dpmanek@gmail.com>
2022-11-30 16:05:03 -0500

    homepage changes

--- src/Components/Homepage/Carousel.js
+++ src/Components/Homepage/Carousel.js
@@ -1,79 +1,70 @@
 import Carousel from 'react-bootstrap/Carousel';
-import "./Carousel.css";
+import './Carousel.css';
 import { useNavigate } from 'react-router-dom';
 
-
 function BackgroundCSL() {
-    let navigate = useNavigate();
+	let navigate = useNavigate();
 	const redirectRoute = (path) => {
-		navigate(path);};
-    return (
-    
-    <Carousel fade>
-      <Carousel.Item interval={1200}>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
-          height = '100%'
-          width= '100%'
-          alt="First slide"
-        />
-        <button
-            className="button1"
-            onClick={() => redirectRoute('/signup')}>   
-            Join Now
-        </button>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+		navigate(path);
+	};
+	return (
+		<Carousel fade>
+			<Carousel.Item interval={1200}>
+				<img
+					className="d-block w-100"
+					src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
+					height="100%"
+					width="100%"
+					alt="First slide"
+				/>
+				{/* <button className="button1" onClick={() => redirectRoute('/signup')}>
+					Join Now
+				</button> */}
+				<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
 
-      </Carousel.Item>
+			<Carousel.Item interval={1200}>
+				<img
+					className="d-block w-100"
+					src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
+					height="100%"
+					width="100%"
+					alt="Second slide"
+				/>
 
-      <Carousel.Item interval={1200}>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
-          height = '100%'
-          width= '100%'
-          alt="Second slide"
-        />
-        <button
-            className="button1"
-            onClick={() => redirectRoute('/signup')}>   
-            Join Now
-        </button>
+				{/* <button className="button1" onClick={() => redirectRoute('/signup')}>
+					Join Now
+				</button> */}
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+				<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
 
-      <Carousel.Item interval={1200}>
-        <img
-          className="d-block w-100"
-          src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
-          height = '100%'
-          width= '100%'
-          alt="Third slide"
-        />
-        <button
-            className="button1"
-            onClick={() => redirectRoute('/signup')}>   
-            Join Now
-        </button>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-
-
+			<Carousel.Item interval={1200}>
+				<img
+					className="d-block w-100"
+					src="https://i.ibb.co/HFd80NP/wp2529605-16k-wallpapers.jpg"
+					height="100%"
+					width="100%"
+					alt="Third slide"
+				/>
+				{/* <button className="button1" onClick={() => redirectRoute('/signup')}>
+					Join Now
+				</button> */}
+				<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
 }
 
 export default BackgroundCSL;
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,114 +1,93 @@
 import React from 'react';
 import BackgroundCSL from './Carousel';
-import {
-	
-	getDocs,
-	
-  } from "firebase/firestore";
-  import { useEffect } from "react";
-  import { useState } from "react";
-  import { Link } from "react-router-dom";
-  import ListingItem from "../Listingitem/Listingitem.jsx";
-  //import Slider from "../components/Slider";
-  import { db } from "../../firebase";
-  import queries from '../../query';
+import { getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ListingItem from '../Listingitem/Listingitem.jsx';
+//import Slider from "../components/Slider";
+import { db } from '../../firebase';
+import queries from '../../query';
+import '../Style/style.css';
 
 const Homepage = () => {
 	const [offerListings, setOfferListings] = useState(null);
 	const [rentListings, setRentListings] = useState(null);
 	const [saleListings, setSaleListings] = useState(null);
 
-	const cleanData = (array)=>{
+	const cleanData = (array) => {
 		const listings = [];
-			array.forEach((doc) => {
-			  return listings.push({
+		array.forEach((doc) => {
+			return listings.push({
 				id: doc.id,
 				data: doc.data(),
-			  });
 			});
-			return listings
-	}
+		});
+		return listings;
+	};
 
 	useEffect(() => {
 		async function fetchListings() {
-		  try {
-			
-			
-			// execute the query
-			const querySnapSale = await getDocs(queries.saleListingQuery);
-			const querySnapRent = await getDocs(queries.rentListingQuery);
-			const querySnapOffer = await getDocs(queries.offerListingQuery);
-			
-			//Calling the clean function for all the data
-			const saleListing = cleanData(querySnapSale)
-			const rentListing = cleanData(querySnapRent)
-			const offerListing = cleanData(querySnapOffer)
-			
-			setOfferListings(offerListing)
-			setRentListings(rentListing)
-			setSaleListings(saleListing);
-		  } catch (error) {
-			console.log(error);
-		  }
+			try {
+				// execute the query
+				const querySnapSale = await getDocs(queries.saleListingQuery);
+				const querySnapRent = await getDocs(queries.rentListingQuery);
+				const querySnapOffer = await getDocs(queries.offerListingQuery);
+
+				//Calling the clean function for all the data
+				const saleListing = cleanData(querySnapSale);
+				const rentListing = cleanData(querySnapRent);
+				const offerListing = cleanData(querySnapOffer);
+
+				setOfferListings(offerListing);
+				setRentListings(rentListing);
+				setSaleListings(saleListing);
+			} catch (error) {
+				console.log(error);
+			}
 		}
 		fetchListings();
-	  }, []);
-	
+	}, []);
+
 	return (
-		<>	
-			<BackgroundCSL/>
+		<>
+			<BackgroundCSL />
 			{/*Slider  */}
 
-			
 			{offerListings && offerListings.length > 0 && (
-          <div>
-            <h2>Recent offers</h2>
-            <Link to="/offers">
-              <p>
-                Show more offers
-              </p>
-            </Link>
-            
-			<ListingItem
-                  data={offerListings}
-				  from="HOMEPAGE"
-                />
-            
-          </div>
-        )}
-		
-        {rentListings && rentListings.length > 0 && (
-          <div >
-            <h2 >Places for rent</h2>
-            <Link to="/category/rent">
-              <p>
-                Show more places for rent
-              </p>
-            </Link>
-            <ListingItem
-                  data={rentListings}
-				  from="HOMEPAGE"
-                />
-          </div>
-        )}
-        {saleListings && saleListings.length > 0 && (
-             <div >
-			 <h2 >Places for sale</h2>
-			 <Link to="/category/rent">
-			   <p>
-				 Show more places for sale
-			   </p>
-			 </Link>
-			 <ListingItem
-				   data={saleListings}
-				   from="HOMEPAGE"
-				 />
-		   </div>
-		 )}
+				<div className="bgcolor">
+					<div className="heading">
+						<h2>Recent offers</h2>
+						<Link to="/offers" style={{ textDecoration: 'none' }}>
+							<p>Show more offers</p>
+						</Link>
+					</div>
+					<ListingItem data={offerListings} from="HOMEPAGE" />
+				</div>
+			)}
 
-
-		
-
+			{rentListings && rentListings.length > 0 && (
+				<div className="bgcolor">
+					<div className="heading">
+						<h2>Places for rent</h2>
+						<Link to="/category/rent" style={{ textDecoration: 'none' }}>
+							<p>Show more places for rent</p>
+						</Link>
+					</div>
+					<ListingItem data={rentListings} from="HOMEPAGE" />
+				</div>
+			)}
+			{saleListings && saleListings.length > 0 && (
+				<div className="bgcolor">
+					<div className="heading">
+						<h2>Places for sale</h2>
+						<Link to="/category/rent" style={{ textDecoration: 'none' }}>
+							<p>Show more places for sale</p>
+						</Link>
+					</div>
+					<ListingItem data={saleListings} from="HOMEPAGE" />
+				</div>
+			)}
 		</>
 	);
 };
