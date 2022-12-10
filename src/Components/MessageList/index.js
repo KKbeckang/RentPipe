d3a7a57097f4c9c7ee9cf647915a6d6f62060daa
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-12-09 20:48:35 -0500

    messaging page done

--- src/Components/MessageList/index.js
+++ src/Components/MessageList/index.js
@@ -2,21 +2,44 @@ import React, {useEffect, useState} from 'react';
 import Compose from '../Compose';
 import Toolbar from '../Toolbar';
 import ToolbarButton from '../ToolbarButton';
+import { db } from "../../firebase";
 import Message from '../Message';
 import moment from 'moment';
 import { IoIosInformationCircle } from "react-icons/io";
+import { getAuth } from "firebase/auth";
 import './MessageList.css';
 import styled from 'styled-components';
 import { Stack } from 'react-bootstrap';
 import PopUp from '../PopUps/PopUp';
 import Modal from 'react-bootstrap/Modal';
 import Listingitem from '../Listingitem/Listingitem';
+import useUser from '../../hooks/useUser';
+import timeAgo from 'epoch-timeago';
+import bed from "../../Assets/bed.png"
+import bathroom from "../../Assets/bathroom.png"
+import Image from 'react-bootstrap/Image'
+import Card from "react-bootstrap/Card"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import rent from "../../Assets/rent.png"
+import sale from "../../Assets/sale.png"
+import Slider from "../Listing/slider";
+import Button from "react-bootstrap/Button"
+import Badge from "react-bootstrap/Badge"
+import { useNavigate } from "react-router-dom";
+
 
 
 const MY_USER_ID = 'apple';
 
 export default function MessageList(props) {
+  const navigate = useNavigate()
   const [messages, setMessages] = useState([])
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+
 
   useEffect(() => {
     getMessages();
@@ -198,7 +221,8 @@ const [rentListings, setRentListings] = useState(null);
         
        <Stack>
         <Toolbar
-          title="fName lName"
+          title="John Smith"
+          subtitle="52 Cambridge Ave - Rent"
           rightItems={[       
           <Button onClick={handleShow}>
             <IoIosInformationCircle size={30}/>
@@ -209,7 +233,41 @@ const [rentListings, setRentListings] = useState(null);
         <Modal.Header closeButton>
           <Modal.Title>Property Information</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Property from firebase goes here</Modal.Body>
+        <Modal.Body>
+       
+        <div>
+        <p className="ptag">52 Cambridge Ave<Badge pill bg="success">Discounted Price</Badge>
+        </p>
+        
+          <Slider img={["https://firebasestorage.googleapis.com/v0/b/rentpipe-d4b1c.appspot.com/o/ELliDGPYACMiiSeWaMsEaerI1mb2-066f1d01acaa436239154cd89c018d52-cc_ft_768.webp-492e4d5b-c7b2-4d84-aac6-e2be84337e64?alt=media&token=7f021ae4-5452-488c-bd8e-28f99255c1e7","https://firebasestorage.googleapis.com/v0/b/rentpipe-d4b1c.appspot.com/o/ELliDGPYACMiiSeWaMsEaerI1mb2-f23e90c99a646d23ba9cef4a255a136e-cc_ft_768.webp-571da05c-30d7-40b9-8792-d3af2c2e31c0?alt=media&token=97c03401-302f-474d-9a7e-ecb61f74a23c","https://firebasestorage.googleapis.com/v0/b/rentpipe-d4b1c.appspot.com/o/ELliDGPYACMiiSeWaMsEaerI1mb2-6d0272ffe043eb5bbbe3a1120db2c29e-cc_ft_768.webp-1cb17f2b-aae2-4092-8fce-a865e73ae8de?alt=media&token=340c9caf-9f23-4223-9837-c75b4d894850","https://firebasestorage.googleapis.com/v0/b/rentpipe-d4b1c.appspot.com/o/ELliDGPYACMiiSeWaMsEaerI1mb2-ef8f961e586be6bda5fd5e4b83b02d81-cc_ft_1536.webp-25210523-7e44-4341-a277-dd45a529bef6?alt=media&token=457192a5-2ee2-4dbd-ba9e-fc2beadf6e5c","https://firebasestorage.googleapis.com/v0/b/rentpipe-d4b1c.appspot.com/o/ELliDGPYACMiiSeWaMsEaerI1mb2-b07ea3d7d9520f76522605261d6feae5-cc_ft_768.webp-f243896b-6f46-4d7e-abad-ffea2363341d?alt=media&token=fc03bc1a-13be-4c1b-aa50-d42600710f06"]}/>
+         <br/>
+        
+          <Container>
+          <Row>
+            <Col><p >Address: 52 Cambridge Ave, Jersey City, NJ 07307</p>
+            </Col>
+            <Col><a  href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent("52 Cambridge Ave, Jersey City, NJ 07307")}`} target="_blank">Open in Google maps</a>
+            </Col>
+          </Row>
+          <Row>
+            <Col><p className="ptag">posted 2 days ago</p>
+            </Col>
+            <Col><Button 
+                onClick={() => {navigate("/rent/yIgDrAAoIyrjv9JaQ3Ex")}}
+              >
+                Show more
+              </Button>
+            </Col>
+          </Row>
+          </Container>
+         
+              
+         
+        
+              
+        </div>
+      
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
--- src/Components/Toolbar/index.js
+++ src/Components/Toolbar/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Toolbar.css';
 
 export default function Toolbar(props) {
-    const { title, leftItems, rightItems } = props;
+    const { title, leftItems, rightItems ,subtitle} = props;
     return (
       <div className="toolbar">
         <div className="left-items">{ leftItems }</div>
+        <div>
         <h1 className="toolbar-title">{ title }</h1>
+        <h1 className="toolbar-title" >{subtitle}</h1>
+        </div>
+        
         <div className="right-items">{ rightItems }</div>
       </div>
     );
