db180d6b7926ee9471ffb1080351ec9ef2bb0404
KKbeckang <beckang@bu.edu>
2022-12-12 20:37:13 -0500

    Navigationbar
    NavBar Changed

--- src/Components/layout/Navbar/Navbar.js
+++ src/Components/layout/Navbar/Navbar.js
@@ -1,82 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import Badge from 'react-bootstrap/esm/Badge';
-
+import Badge from "react-bootstrap/esm/Badge";
 
 function Navbar() {
-
-  const [isLoggedin, setIsLoggedIn] = useState(false) 
+  const [isLoggedin, setIsLoggedIn] = useState(false);
   const [pageState, setPageState] = useState("Sign in");
   const navigate = useNavigate();
   const auth = getAuth();
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setPageState("Account");
+      } else {
+        setIsLoggedIn(false);
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
 
-	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-		if (user) {
-			setIsLoggedIn(true)
-			setPageState("Account");
-		} else {
-			setIsLoggedIn(false)
-		  setPageState("Sign in");
-		}
-	  });
-	}, [auth]);
+  return (
+    <nav>
 
+        <Link className="" to="/">
+		<a href="https://ibb.co/PYSgMhH"><img src="https://i.ibb.co/wLv47Wm/Rent-Pipe-logos-white-adobe-express.png" alt="Rent-Pipe-logos-white-adobe-express" border="0" height="75px"/></a>
 
-	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-color py-0 ">
-			<div className="container-fluid width-change">
-				
-				<Link className="navbar-brand hover-change" to="/">
-				<h1>Rentpipe</h1>
-				</Link>
-
-				<form className="d-flex">
-						
-						<button
-								className="btn btn-outline-success navbar-success button-fix"
-								type="button"
-								onClick={() => navigate('/category/rent')} 
-							>
-								Rent
-							</button>
-							<button
-								className="btn btn-outline-success navbar-success button-fix"
-								type="button"
-								onClick={() => navigate('/category/sale')} 
-							>
-								Sale
-							</button>
-						
-
-							{/*For a Button that should be displayed only when the user is logged in  */}
-					
-					
-							{isLoggedin? <button
-								className="btn btn-outline-success navbar-success button-fix"
-								type="button"
-								onClick={() => navigate('/messenger')} 
-							>
-								Messages 
-								{isLoggedin? <Badge  bg="danger">2</Badge>:null}
-							</button>: null}
-
-								<button
-									className="btn btn-outline-success navbar-success button-fix"
-									type="button"
-									onClick={() => { isLoggedin?  navigate('/profile'):navigate('/login')}}
-								>
-									{pageState}
-								</button>
-							
-						
-				</form>
-			</div>
-		</nav>
-	);
-} 
+        </Link>
+		<div>
+		<ul id="navbar">
+			<li><a className="" to="/" href="/">Home</a></li>
+			<li><a className="" to="/category/rent" href="/category/rent">Rent</a></li>
+			<li><a className="" to="/category/sale" href="/category/sale">Sale</a></li>
+			{isLoggedin? <li><a href='javascript:void(0)' onClick={() => navigate('/messenger')} >Messages {isLoggedin? <Badge  bg="danger">2</Badge>:null}</a></li>:null}
+			{isLoggedin? <li><a href='javascript:void(0)' onClick={() => { isLoggedin?  navigate('/profile'):navigate('/login')}} >{pageState}</a></li>:null}
+		</ul>
+		</div>
+    </nav>
+  );
+}
 
 export default Navbar;
